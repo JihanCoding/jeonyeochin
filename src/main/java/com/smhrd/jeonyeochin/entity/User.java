7de7b8d2375ef5cca5822746bc2382757328c333
Wan_Dk <ttyl0713@gmail.com>
2025-06-16 10:00:12 +0900

    감태완 user.java 주석달기

--- src/main/java/com/smhrd/jeonyeochin/entity/User.java
+++ src/main/java/com/smhrd/jeonyeochin/entity/User.java
@@ -1,8 +1,14 @@
 package com.smhrd.jeonyeochin.entity;
-import jakarta.persistence.*;
-import lombok.*;
 
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "tb_user")
@@ -12,23 +18,23 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private Integer userId;
+    private Integer userId; // 사용자 ID
 
     @Column(name = "user_nick", nullable = false, length = 255)
-    private String userNick;
+    private String userNick; // 사용자 닉네임
 
     @Column(name = "user_name", nullable = false, length = 255)
-    private String userName;
+    private String userName; // 사용자 이름
 
     @Column(name = "user_email", nullable = false, length = 255)
-    private String userEmail;
+    private String userEmail; // 사용자 이메일
 
     @Column(name = "user_password", nullable = false, length = 255)
-    private String userPassword;
+    private String userPassword; // 사용자 비밀번호
 
     @Column(name = "user_profile", nullable = false, length = 255)
-    private String userProfile;
+    private String userProfile; // 사용자 프로필 사진 URL
 
     @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt; // 사용자 생성 시간
 }
